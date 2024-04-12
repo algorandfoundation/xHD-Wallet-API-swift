cd060f862fc7822546a1bd113f6a7fa6fbe6419b
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2024-04-12 11:19:11 +0200

    feat: adds rawSign, signAlgoTransaction and verifyWithPublicKey

--- Sources/bip32-ed25519-swift/CryptoUtils.swift
+++ Sources/bip32-ed25519-swift/CryptoUtils.swift
@@ -15,39 +15,48 @@
  * limitations under the License.
  */
 
-import CommonCrypto
+import CryptoKit
 import Foundation
 
 public struct CryptoUtils {
     public static func sha512(data: Data) -> Data {
-        var hash = Data(count: Int(CC_SHA512_DIGEST_LENGTH))
-        data.withUnsafeBytes { dataBytes in
-            _ = hash.withUnsafeMutableBytes { hashBytes in
-                CC_SHA512(dataBytes.baseAddress, CC_LONG(data.count), hashBytes.bindMemory(to: UInt8.self).baseAddress)
-            }
-        }
-        return hash
+        let hashed = SHA512.hash(data: data)
+        return Data(hashed)
     }
 
     public static func hmacSha512(key: Data, data: Data) -> Data {
-        var hmac = Data(count: Int(CC_SHA512_DIGEST_LENGTH))
-        key.withUnsafeBytes { keyBytes in
-            data.withUnsafeBytes { dataBytes in
-                hmac.withUnsafeMutableBytes { hmacBytes in
-                    CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA512), keyBytes.baseAddress, key.count, dataBytes.baseAddress, data.count, hmacBytes.bindMemory(to: UInt8.self).baseAddress)
-                }
-            }
-        }
-        return hmac
+        let key256 = SymmetricKey(data: key)
+        let hmac = HMAC<SHA512>.authenticationCode(for: data, using: key256)
+        return Data(hmac)
     }
 
     public static func sha256(data: Data) -> Data {
-        var hash = Data(count: Int(CC_SHA256_DIGEST_LENGTH))
-        data.withUnsafeBytes { dataBytes in
-            _ = hash.withUnsafeMutableBytes { hashBytes in
-                CC_SHA256(dataBytes.baseAddress, CC_LONG(data.count), hashBytes.bindMemory(to: UInt8.self).baseAddress)
-            }
+        let hashed = SHA256.hash(data: data)
+        return Data(hashed)
+    }
+
+    public static func sha512_256(data: Data) -> Data {
+        return Data(SHA512_256.init().hash([UInt8](data)))
+    }
+
+    public static func encodeAddress(bytes: Data) throws -> String {
+        let lenBytes = 32
+        let checksumLenBytes = 4
+        let expectedStrEncodedLen = 58
+
+        // compute sha512/256 checksum
+        let hash = sha512_256(data: bytes)
+        let hashedAddr = hash[..<lenBytes]  // Take the first 32 bytes
+
+        // take the last 4 bytes of the hashed address, and append to original bytes
+        let checksum = hashedAddr[(hashedAddr.count - checksumLenBytes)...]
+        let checksumAddr = bytes + checksum
+
+        // encodeToMsgPack addr+checksum as base32 and return. Strip padding.
+        let res = Base32.encode(data: checksumAddr).trimmingCharacters(in: ["="])
+        if (res.count != expectedStrEncodedLen) {
+            throw NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "unexpected address length \(res.count)"])
         }
-        return hash
+        return res
     }
 }
